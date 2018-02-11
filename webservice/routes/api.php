040a5fcb8780ec94a89bd8d6dd9346851ba33f87
vinicius monteiro arjonas <viniciusmonteiroarjonas@outlook.com>
2018-02-11 10:39:10 -0200

    Ajuste de rotas api

--- webservice/routes/api.php
+++ webservice/routes/api.php
@@ -1,18 +1,10 @@
 <?php
 
-use Illuminate\Http\Request;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/ofertas', function () {
+	header("Access-control-Allow-Origin: *");
+	$ofertas = App\Oferta::all();
+	foreach ($ofertas as $oferta) {
+		$oferta->imagem = asset($oferta->imagem);
+	}
+	return response()->json($ofertas);
 });
